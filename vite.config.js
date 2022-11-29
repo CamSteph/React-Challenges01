8ccfc47fa20d60b0ff45c276ccbf603b7fca5c21
CamSteph <camstephenson2017@gmail.com>
2022-11-28 20:35:52 -0500

    Implement env variables for fontend

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  },
-})
+export default defineConfig(({command, mode}) => {
+
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    server: {
+      port: env.UI_PORT || 3000,
+    }
+  };
+
+});
